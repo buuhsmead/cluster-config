ff3e917a6c6b072dc690952e174e1fcc76aa6473
buuhsmead <hdaems@redhat.com>
2023-06-20 14:55:56 +0200

    Starting with bootstrap

---
+++ bootstrap.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+
+if [ $# -lt 1 ]; then
+    echo "No overlay specified, please specify an overlay from bootstrap/overlays"
+    exit 1
+else
+    OVERLAY=$1
+    echo "Configuring cluster ${OVERLAY}"
+fi
+
+oc project openshift-gitops
+kustomize build bootstrap/overlays/${OVERLAY} | oc apply -f -
+
